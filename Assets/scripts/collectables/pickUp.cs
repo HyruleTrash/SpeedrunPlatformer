0274cadf11c9ffaf371acea813d8a6425e9146dc
dipstick <87959@glr.nl>
2023-05-30 14:53:21 +0200

    worked further on the seashell

--- Assets/scripts/collectables/pickUp.cs
+++ Assets/scripts/collectables/pickUp.cs
@@ -2,18 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class seaShell : MonoBehaviour
+public class pickUp : MonoBehaviour
 {
     public int seaShellCount = 0;
+    private bool collecting = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("seaShell"))
+        if (collision.gameObject.CompareTag("seaShell") && !collecting)
         {
+            collecting = true;
             Destroy(collision.gameObject);
             seaShellCount++;
             Debug.Log(seaShellCount);
         }
     }
+
+    void Update()
+    {
+        collecting = false;    
+    }
 }
+
 //gesi wii need too cuuk mempf
