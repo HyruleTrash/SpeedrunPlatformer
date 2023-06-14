235aa64a7a4b3d6c95a875d6547f0c3359e88bcc
dipstick <jensouwe@xs4all.nl>
2023-06-14 11:51:38 +0200

    started UI and finished SeaShell

--- Assets/scripts/collectables/pickUp.cs
+++ Assets/scripts/collectables/pickUp.cs
@@ -6,12 +6,19 @@ public class pickUp : MonoBehaviour
 {
     public int seaShellCount = 0;
     private bool collecting = false;
+    public ParticleSystem particle;
+
+    private void Awake()
+    {
+
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("seaShell") && !collecting)
         {
             collecting = true;
+            Instantiate(particle, collision.gameObject.transform.position, Quaternion.identity);
             Destroy(collision.gameObject);
             seaShellCount++;
             Debug.Log(seaShellCount);
