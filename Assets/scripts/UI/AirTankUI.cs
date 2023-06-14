3b8aebce86b70cc096af7d21eb4489d15c36002d
dipstick <jensouwe@xs4all.nl>
2023-06-14 13:06:44 +0200

    added airtank functionality

--- Assets/scripts/UI/AirTankUI.cs
+++ Assets/scripts/UI/AirTankUI.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class AirTankUI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private const float MAX_OXY = 100f;
+    public float oxy = MAX_OXY;
+    private Image oxybar;
+
+    public float decreaseAmount = 1f;
+
+    private float lastDecreaseTime;
+
+
     void Start()
     {
-        
+        oxybar = GetComponent<Image>();
+        lastDecreaseTime = Time.time; 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        oxybar.fillAmount = oxy / MAX_OXY;
+
+        if (Time.time - lastDecreaseTime >= 0.5f)
+        {
+            oxy = oxy - decreaseAmount;
+            lastDecreaseTime = Time.time; // Update the last decrease time
+        }
     }
 }
