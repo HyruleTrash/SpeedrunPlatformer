9ee74265e22a2e3b3011ebb9e3c50314fd17778f
dipstick <jensouwe@xs4all.nl>
2023-06-14 14:17:08 +0200

    main menu functionality

---
+++ Assets/scripts/AppHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+[System.Serializable]
+public static class AppHelper
+{
+    #if UNITY_WEBPLAYER
+    public static string webplayerQuitURL = "http://google.com";
+    #endif
+    public static void Quit()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #elif UNITY_WEBPLAYER
+        Application.OpenURL(webplayerQuitURL);
+        #else
+        Application.Quit();
+        #endif
+    }
+}
